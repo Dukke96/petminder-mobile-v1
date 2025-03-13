6c7db4b565a3cc15ee1eec3c857a54d8fc91b992
Dukke96 <79385509+Dukke96@users.noreply.github.com>
2025-03-13 21:42:09 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/(with-layout)/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+import { MobileNavbar } from "@/components/layout/navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PetMinder",
+  description: "PetMinder",
+  manifest: "/manifest.json",
+  icons: {
+    apple: "/petminder-512x512.png",
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased mb-20`}
+      >
+        <img
+          src="/login-background.webp"
+          alt="Background"
+          className="fixed top-0 left-0 w-full h-full object-cover -z-10 pointer-events-none"
+        />
+        <MobileNavbar />
+        {children}
+      </body>
+    </html>
+  );
+}
